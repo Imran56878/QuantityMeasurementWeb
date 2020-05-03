ec9a1e9565b29f4736d988818d504bb8b9ae70a6
Imran56878 <mi056878>
2020-05-03 19:05:35 +0530

    [Imran]Added Security Repository_Folder_Classess

---
+++ ParkingLotRepository/Security/ISecurityRepo.cs
@@ -0,0 +1,14 @@
+﻿using ParkingLotModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotRepository.Security
+{
+   public interface ISecurityRepo
+    {
+        Task<int> PoliceParking(ParkingModel parkingModel);
+        string ParkingStatus();
+    }
+}
---
+++ ParkingLotRepository/Security/ImpSecurityRepo.cs
@@ -0,0 +1,54 @@
+﻿using ParkingLotModel;
+using ParkingLotRepository.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotRepository.Security
+{
+    public class ImpSecurityRepo : ISecurityRepo
+    {
+        private readonly UserDBContext userDBContext;
+        public ImpSecurityRepo(UserDBContext userDBContext)
+        {
+            this.userDBContext = userDBContext;
+        }
+
+        public string ParkingStatus()
+        {
+            var totalRecord = userDBContext.Parking.Count();
+            var a = userDBContext.Parking;
+            var s = a.OrderByDescending(p => p.ParkingSlotNumber).FirstOrDefault().ParkingSlotNumber;
+            if (totalRecord == s)
+                return "Status: Slot is Full";
+            return "Status: Slot is not Full";
+        }
+
+        public Task<int> PoliceParking(ParkingModel parkingModel)
+        {
+            parkingModel.TimeIn = DateTime.Now;
+            parkingModel.TimeOut = DateTime.MinValue;
+            var count = userDBContext.Parking.Count();
+            var slotNumber = parkingModel.ParkingSlotNumber;
+            for (int i = 1; i <= count + 2; i++)
+            {
+
+                if (userDBContext.Parking.Find(slotNumber) == null)
+                {
+                    var result = userDBContext.Parking.Add(parkingModel);
+
+                    break;
+                }
+                else
+                {
+                    slotNumber = i;
+                    parkingModel.ParkingSlotNumber = slotNumber;
+                }
+
+            }
+            return userDBContext.SaveChangesAsync();
+        }
+    }
+}
